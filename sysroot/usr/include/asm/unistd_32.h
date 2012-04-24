9e754ea754eac37d1cd20182cc5d37df85239b0d
Andrew Hsieh <andrewhsieh@google.com>
2012-04-24 11:36:01 +0800

    Added missing defines and headers
    Added more defines and headers to make this prebuilt capable of compiling
    all host binaries in Android.
    
    1. Added more defines in sysroot/usr/include/asm/unistd_32.h, from
       linux-libc-dev_2.6.32-21.32_i386.deb (lucid)
    2. Added more magic numbers in sysroot/usr/include/linux/magic.h,
       also from linux-libc-dev_2.6.32-21.32_i386.deb (lucid)
    3. Added new sub-dir sysroot/usr/include/sound, from
       linux-headers-2.6.24-16_2.6.24-16.30_all.deb (hardy)
    
    build-hardy-toolchain.sh will be enhanced later to automate this
    
    Change-Id: I850ae0767b968b6f69d1d7c1d4922fdbe4b7c514

--- sysroot/usr/include/asm/unistd_32.h
+++ sysroot/usr/include/asm/unistd_32.h
@@ -328,7 +328,20 @@
 #define __NR_utimensat		320
 #define __NR_signalfd		321
 #define __NR_timerfd		322
+#define __NR_timerfd_create	322
 #define __NR_eventfd		323
 #define __NR_fallocate		324
+#define __NR_timerfd_settime	325
+#define __NR_timerfd_gettime	326
+#define __NR_signalfd4		327
+#define __NR_eventfd2		328
+#define __NR_epoll_create1	329
+#define __NR_dup3		330
+#define __NR_pipe2		331
+#define __NR_inotify_init1	332
+#define __NR_preadv		333
+#define __NR_pwritev		334
+#define __NR_rt_tgsigqueueinfo	335
+#define __NR_perf_event_open	336
 
 #endif /* _ASM_I386_UNISTD_H_ */
